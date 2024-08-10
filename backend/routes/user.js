a38021a605f884c32945545171b415aef26521bc
Nabadeep <nabadeepdas069@gmail.com>
2024-08-10 17:33:39 +0530

    auth update

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,45 @@
 const express = require("express");
-const userRouter = require("./user");
+const { User } =  require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = require("../config");
+const zod = require("zod");
+const router = express.Router();
 
-const router = expres.Router();
+const signupSchema = zod.object({
+    username: zod.string(),
+    password: zod.string(),
+    firstname: zod.string(),
+    password: zod.string()
+})
 
-router.use("/user", userRouter);
+
+router.post("/signup", async(req,res) => {
+    const body = req.body;
+    const {success} = signupSchema.safeParse(req.body);
+    if (!success) {
+        return res.json({
+            message: "email is already taken"
+        })
+    }
+
+    const user = User.findone({
+         username: body.username
+    })
+    if (user._id) {
+        return res.json({
+            message: "email is already taken"
+        })
+    }
+
+    const dbuser = await user.Create(body);
+    const token = jwt.sign({
+        userId: dbUser._id
+    }, JWT_SECRET)
+   res.json({
+    message: "user created successfully",
+    token: token
+   })
+})
+
 
 module.exports = router;
