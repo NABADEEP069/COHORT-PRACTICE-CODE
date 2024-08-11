25bd62096d1be757a340190736a4aec54c1222e8
Nabadeep <nabadeepdas069@gmail.com>
2024-08-11 10:14:33 +0530

    update

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+const JWT_SECRET = "NABA";
+module.exports = JWT_SECRET
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const userRouter = require("./user");
+
 const router =express.Router();
 const cors = require("cors");
 
