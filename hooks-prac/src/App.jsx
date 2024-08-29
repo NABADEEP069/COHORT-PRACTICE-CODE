da205e54cfae9db507980c59dffe2612a5f1f2c0
Nabadeep <nabadeepdas069@gmail.com>
2024-08-29 08:32:26 +0530

    start hooks

--- hooks-prac/src/App.jsx
+++ hooks-prac/src/App.jsx
@@ -1,15 +1,39 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-function App() {
+function useTodos(n) {
   const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    const value = setInterval(() => {
+      axios.get("https://sum-server.100xdevs.com/todos")
+        .then(res => {
+          setTodos(res.data.todos);
+          setLoading(false);
+        })
+    }, n * 1000)
+  
     axios.get("https://sum-server.100xdevs.com/todos")
       .then(res => {
         setTodos(res.data.todos);
+        setLoading(false);
       })
-  }, [])
+
+    return () => {
+      clearInterval(value)
+    }
+  }, [n])
+
+  return {todos, loading};
+}
+
+function App() {
+  const {todos, loading} = useTodos(10);
+
+  if (loading) {
+    return <div> loading... </div>
+  }
 
   return (
     <>
@@ -24,6 +48,6 @@ function Track({ todo }) {
     <br />
     {todo.description}
   </div>
-}   
+}
 
 export default App
