d1291de7936ede3f94da78b76268da2338549fb8
Nabadeep <nabadeepdas069@gmail.com>
2024-09-21 19:30:57 +0530

    intro of prisma

--- class-9.2/a.ts
+++ class-9.2/a.ts
@@ -1,2 +1,13 @@
-const x: number = 11;
-console.log(x);
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+function runAfter1S(fn: () => void) {
+    setTimeout(fn, 1000);
+}
+
+ runAfter1S(function() {
+    console.log("naba");
+})
+
+ 
